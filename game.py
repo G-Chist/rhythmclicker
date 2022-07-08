a4e1e9a53beb58b9e8bc25ada98a95957c1890e3
G-Chist <42654606+G-Chist@users.noreply.github.com>
2022-07-08 18:16:12 +0800

    Update game.py

--- game.py
+++ game.py
@@ -1,5 +1,12 @@
-from exterior import *
+from menu import *
+
+summon_menu()
+
+from clickerexterior import *
 from notefunctions import *
 from songanalyzer import *
+from menu import *
 
 playGame(gamearr=timearr, timeout=times[-1], waiting=2)
+
+print("Game finished!")
